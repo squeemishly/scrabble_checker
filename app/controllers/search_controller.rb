b7f70fe400b67a2af21881302bc9bb8c64a68ca7
squeemishly <squeemishly@gmail.com>
2017-10-06 16:35:37 -0600

    Creates Faraday call to Oxford dictionaries Installs Figaro gem and sets up environment variables for Oxford dictionary api keys
     Please enter the commit message for your changes. Lines starting

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,11 @@
 class SearchController < ApplicationController
   def index
-    
+    conn = Faraday.new(url: "https://od-api.oxforddictionaries.com:443")
+    response = conn.get "/api/v1/entries/en/best",
+                        {},
+                        {
+                          'app_id' => ENV['oxford_dictionary_app_id'],
+                          'app_key' => ENV['oxford_dictionary_app_key']
+                        }
   end
 end
