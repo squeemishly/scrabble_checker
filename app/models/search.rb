515d717cd471561e597214ec2f06f10eabe2ea74
squeemishly <squeemishly@gmail.com>
2017-10-09 15:32:19 -0600

    moves logic related to Faraday call to the oed_service

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,13 +1,6 @@
-class Search < ActiveRecord::Base
+class Search
   def self.find_word(word)
-    conn = Faraday.new(url: "https://od-api.oxforddictionaries.com:443")
-    response = conn.get "/api/v1/entries/en/#{word}",
-                    {},
-                    {
-                      'app_id' => ENV['oxford_dictionary_app_id'],
-                      'app_key' => ENV['oxford_dictionary_app_key']
-                    }
-    # response = OedService.find_word(word)
+    response = OedService.find_word(word)
     res = JSON.parse(response.body)
     self.build_defs(res)
   end
