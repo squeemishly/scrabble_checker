5b92a12373606d5c7c685271a9053cb6c2558fb1
squeemishly <squeemishly@gmail.com>
2017-10-10 15:55:59 -0600

    refactors api cll logic out to it's own method

--- app/services/Oed_Service.rb
+++ app/services/Oed_Service.rb
@@ -1,6 +1,10 @@
 class OedService
   def self.find_word(word)
     conn = Faraday.new(url: "https://od-api.oxforddictionaries.com:443")
+    self.oed_call(conn, word)
+  end
+
+  def self.oed_call(conn, word)
     conn.get "/api/v1/entries/en/#{word}",
               {},
               {
