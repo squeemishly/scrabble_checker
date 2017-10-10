3fe3768766334df435e6894dce785621026394e4
squeemishly <squeemishly@gmail.com>
2017-10-10 16:26:31 -0600

    writes test for private method oed_call

--- spec/services/oed_service_spec.rb
+++ spec/services/oed_service_spec.rb
@@ -41,5 +41,22 @@ RSpec.describe OedService do
         expect(results["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0]).to be_a String
       end
     end
+
+    it '#oed_call' do
+      VCR.use_cassette('oed_service_oed_call') do
+        oed = OedService.new
+        url = "https://od-api.oxforddictionaries.com:443"
+        res = oed.instance_eval { oed_call(Faraday.new(url: url), "third") }
+        results = JSON.parse(res.body)
+        expect(results).to be_a Hash
+        expect(results).to have_key "results"
+        expect(results["results"][0]).to have_key "lexicalEntries"
+        expect(results["results"][0]["lexicalEntries"][0]).to have_key "entries"
+        expect(results["results"][0]["lexicalEntries"][0]["entries"][0]).to have_key "senses"
+        expect(results["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]).to have_key "definitions"
+        expect(results["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"]).to be_a Array
+        expect(results["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0]).to be_a String
+      end
+    end
   end
 end
