e95902d4232f405bfa2c2534790d0dae2c217508
squeemishly <squeemishly@gmail.com>
2017-10-10 15:40:33 -0600

    refactors out logic to dig into each entry to find definitions

--- app/models/search.rb
+++ app/models/search.rb
@@ -10,11 +10,16 @@ class Search
   def self.build_defs(res)
     arr = []
     res['results'].first['lexicalEntries'].each do |lex_entry|
-      lex_entry["entries"].each do |entry|
-        self.find_defs(entry, arr)
-      end
+      self.find_entries(lex_entry, arr)
     end
     arr.flatten.compact
+  end
+
+  def self.find_entries(lex_entry, arr)
+    lex_entry["entries"].each do |entry|
+      self.find_defs(entry, arr)
+    end
+    arr
   end
 
   def self.find_defs(entry, arr)
