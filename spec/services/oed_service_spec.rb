214f8b5c54d452fa390ca7046e9277db72299875
squeemishly <squeemishly@gmail.com>
2017-10-10 16:21:11 -0600

    writes tests for public methods on OedService

--- spec/services/oed_service_spec.rb
+++ spec/services/oed_service_spec.rb
@@ -7,4 +7,39 @@ RSpec.describe OedService do
       expect(oed.url).to eq "https://od-api.oxforddictionaries.com:443"
     end
   end
+
+  context 'class methods' do
+    it '.find_word' do
+      VCR.use_cassette('oed_service_class_find_word') do
+        res = OedService.find_word("test")
+        oed = JSON.parse(res.body)
+        expect(oed).to be_a Hash
+        expect(oed).to have_key "results"
+        expect(oed["results"][0]).to have_key "lexicalEntries"
+        expect(oed["results"][0]["lexicalEntries"][0]).to have_key "entries"
+        expect(oed["results"][0]["lexicalEntries"][0]["entries"][0]).to have_key "senses"
+        expect(oed["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]).to have_key "definitions"
+        expect(oed["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"]).to be_a Array
+        expect(oed["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0]).to be_a String
+      end
+    end
+  end
+
+  context 'instance methods' do
+    it '#find_word' do
+      VCR.use_cassette('oed_service_instance_find_word') do
+        oed = OedService.new
+        res = oed.find_word("another")
+        results = JSON.parse(res.body)
+        expect(results).to be_a Hash
+        expect(results).to have_key "results"
+        expect(results["results"][0]).to have_key "lexicalEntries"
+        expect(results["results"][0]["lexicalEntries"][0]).to have_key "entries"
+        expect(results["results"][0]["lexicalEntries"][0]["entries"][0]).to have_key "senses"
+        expect(results["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]).to have_key "definitions"
+        expect(results["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"]).to be_a Array
+        expect(results["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0]).to be_a String
+      end
+    end
+  end
 end
