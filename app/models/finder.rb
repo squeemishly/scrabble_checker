eafacecf16c6d123aa9fd1afb346cad55fb24cce
squeemishly <squeemishly@gmail.com>
2017-10-15 11:28:53 -0600

    moves logic components into their own methods

--- app/models/finder.rb
+++ app/models/finder.rb
@@ -1,5 +1,16 @@
 class Finder
   def self.all_words(tiles)
+    self.anagramify(tiles).keep_if { |word| word.length > 1 }
+  end
+
+  def self.anagramify(tiles)
+    self.make_list(tiles).flatten(1).map do |letters|
+      response = Faraday.get("http://www.anagramica.com/best/#{letters.join('')}")
+      JSON.parse(response.body)["best"]
+    end.flatten
+  end
+
+  def self.make_list(tiles)
     tiles_list = tiles.split('')
     tiles_combined = []
     i = 2
@@ -7,9 +18,6 @@ class Finder
       tiles_combined << tiles_list.combination(i).to_a
       i += 1
     end
-    tiles_combined.flatten(1).map do |letters|
-      response = Faraday.get("http://www.anagramica.com/best/#{letters.join('')}")
-      JSON.parse(response.body)["best"]
-    end.flatten.keep_if { |word| word.length > 1 }
+    tiles_combined
   end
 end
