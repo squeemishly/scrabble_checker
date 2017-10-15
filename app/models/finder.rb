3aa4f308d9d4e7942ff3f82254e358c2b4e445ba
squeemishly <squeemishly@gmail.com>
2017-10-15 11:21:37 -0600

    removed if/else logic for empty string

--- app/models/finder.rb
+++ app/models/finder.rb
@@ -1,20 +1,15 @@
 class Finder
   def self.all_words(tiles)
-    if tiles == ''
-      []
-    else
-      result = []
-      tiles_list = tiles.split('')
-      tiles_combined = []
-      i = 2
-      while i <= tiles_list.length do
-        tiles_combined << tiles_list.combination(i).to_a
-        i += 1
-      end
-      thing = tiles_combined.flatten(1).map do |letters|
-        response = Faraday.get("http://www.anagramica.com/best/#{letters.join('')}")
-        JSON.parse(response.body)["best"]
-      end.flatten.keep_if { |word| word.length > 1 }
+    tiles_list = tiles.split('')
+    tiles_combined = []
+    i = 2
+    while i <= tiles_list.length do
+      tiles_combined << tiles_list.combination(i).to_a
+      i += 1
     end
+    tiles_combined.flatten(1).map do |letters|
+      response = Faraday.get("http://www.anagramica.com/best/#{letters.join('')}")
+      JSON.parse(response.body)["best"]
+    end.flatten.keep_if { |word| word.length > 1 }
   end
 end
