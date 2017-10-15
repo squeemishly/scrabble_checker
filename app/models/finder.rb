f7b10ac50907d1c77e9119981ddf882da3a4a4a2
squeemishly <squeemishly@gmail.com>
2017-10-15 11:04:26 -0600

    .all_words can return a list of words from 3 tiles

--- app/models/finder.rb
+++ app/models/finder.rb
@@ -3,8 +3,18 @@ class Finder
     if tiles == ''
       []
     else
-      response = Faraday.get("http://www.anagramica.com/best/#{tiles}")
-      JSON.parse(response.body)["best"]
+      result = []
+      tiles_list = tiles.split('')
+      tiles_combined = []
+      i = 2
+      while i <= tiles_list.length do
+        tiles_combined << tiles_list.combination(i).to_a
+        i += 1
+      end
+      thing = tiles_combined.flatten(1).map do |letters|
+        response = Faraday.get("http://www.anagramica.com/best/#{letters.join('')}")
+        JSON.parse(response.body)["best"]
+      end.flatten.keep_if { |word| word.length > 1 }
     end
   end
 end
