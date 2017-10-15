d6f981c41353932ba25ddb48145049afd7ef34ab
squeemishly <squeemishly@gmail.com>
2017-10-15 12:45:13 -0600

    writes test for initialize

--- spec/services/anagramica_service_spec.rb
+++ spec/services/anagramica_service_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe AnagramicaService do
+  let(:ags) { AnagramicaService.new }
+
+  context 'initialize' do
+    it 'has a url' do
+      expect(ags.url).to eq "http://www.anagramica.com"
+    end
+  end
+
   context '.get_words' do
     it 'returns a list of anagrams for 2 letters' do
       VCR.use_cassette 'anagramica_service_get_words_2_letters' do
@@ -12,5 +20,17 @@ RSpec.describe AnagramicaService do
         expect(words_list["best"]).to eq ['no', 'on']
       end
     end
+
+    it 'returns a list of 4 letter words for 4 tiles' do
+      VCR.use_cassette 'anagramica_service_4_letters' do
+        words = AnagramicaService.get_words('note')
+        words_list = JSON.parse(words.body)
+        expect(words_list).to be_a Hash
+        expect(words_list["best"]).to be_a Array
+        expect(words_list["best"].count).to eq 2
+        expect(words_list["best"]).to eq ['note', 'tone']
+      end
+    end
   end
+
 end
