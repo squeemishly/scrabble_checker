93d2f3379855cf5262f73c6c09b769287a566e6c
squeemishly <squeemishly@gmail.com>
2017-10-15 10:35:25 -0600

    writes test for .all_words to return an empty array when it starts with an empty string

---
+++ spec/models/finder_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Finder do
+  context '.all_words' do
+    it 'returns an empty array when it has no tiles' do
+      VCR.use_cassette 'finder_empty_string' do
+        words = Finder.all_words('')
+        expect(words).to eq []
+      end
+    end
+  end
+end
