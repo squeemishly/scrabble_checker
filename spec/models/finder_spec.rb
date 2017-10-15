690c70fdca5281a18f294b194de3d8b82afd0584
squeemishly <squeemishly@gmail.com>
2017-10-15 11:20:23 -0600

    removed test for empty string. will add that logic in the controller

--- spec/models/finder_spec.rb
+++ spec/models/finder_spec.rb
@@ -2,13 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Finder do
   context '.all_words' do
-    it 'returns an empty array when it has no tiles' do
-      VCR.use_cassette 'finder_empty_string' do
-        words = Finder.all_words('')
-        expect(words).to eq []
-      end
-    end
-
     it 'returns all 2 letter anagrams for 2 tiles' do
       VCR.use_cassette 'finder_2_tiles' do
         words = Finder.all_words('no')
@@ -20,8 +13,17 @@ RSpec.describe Finder do
     it 'returns all 2 and 3 letter anagrams for 3 tiles' do
       VCR.use_cassette 'finder_3_tiles' do
         words = Finder.all_words('not')
+        expect(words.all? { |word| word.length > 1 }).to be true
         expect(words.count).to eq 5
         expect(words).to eq ['no', 'on', 'to', 'not', 'ton']
+      end
+    end
+
+    it 'returns all anagrams from 4 tiles' do
+      VCR.use_cassette 'finder_4_tiles' do
+        words = Finder.all_words('arst')
+        expect(words.count).to eq 15
+        expect(words).to eq ["as", "at", "rs", "ts", "as", "art", "rat", "tar", "sat", "rs", "arts", "rats", "star", "tars", "tsar"]
       end
     end
   end
